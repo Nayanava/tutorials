6b993e3f0e95a2cf3bfc36a9f89b26e00bd3fd10
Nayanava <nayanava.de01@gmail.com>
2021-08-17 10:12:16 +0530

    Revert "Nayde/valet parking"

--- persistence-modules/spring-boot-persistence-2/src/main/java/com/parking/spring/oracle/pooling/entity/ParkingStatus.java
+++
@@ -1,8 +0,0 @@
-package com.parking.spring.oracle.pooling.entity;
-
-/**
- * @author nayanava
- */
-public enum ParkingStatus {
-    CHECKED_IN, CHECKED_OUT
-}
--- persistence-modules/spring-boot-persistence-2/src/main/java/com/parking/spring/oracle/pooling/repository/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package com.parking.spring.oracle.pooling.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.parking.spring.oracle.pooling.entity.Book;
-
-public interface BookRepository extends JpaRepository<Book, Long>{		
-
-}
